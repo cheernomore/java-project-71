92b610edb4fc2b53187cf415c494ee3c7132fbd6
Nick Kudryavtsev <ch33rnomore@gmail.com>
2023-05-08 23:14:43 +0300

    Step #3: added show -help for cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Command;
+
+@Command(name="differ", mixinStandardHelpOptions = true, version = "differ 1.0",
+        description = "Return difference between .json-, .yaml- files.")
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    @Parameters(index = "0", paramLabel = "pathToFile1")
+    String pathToFile1;
+    @Parameters(index = "1", paramLabel = "pathToFile2")
+    String pathToFile2;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
