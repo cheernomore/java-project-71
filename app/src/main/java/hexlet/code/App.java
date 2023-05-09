81b581b1d2f4d704f63d878f3beb6d1e252e9ecf
Nick Kudryavtsev <ch33rnomore@gmail.com>
2023-05-09 08:10:49 +0300

    Step #4: added option, changed description

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,19 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
-@Command(name="differ", mixinStandardHelpOptions = true, version = "differ 1.0",
-        description = "Return difference between .json-, .yaml- files.")
+@Command(name="gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class App {
-
-    @Parameters(index = "0", paramLabel = "pathToFile1")
-    String pathToFile1;
-    @Parameters(index = "1", paramLabel = "pathToFile2")
-    String pathToFile2;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "Path to first file")
+    String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "Path to second file")
+    String filepath2;
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "Choose format to compare")
+    String option;
 
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
